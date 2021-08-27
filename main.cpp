dcbe8e9273fc02573a801e9475fa5ac2b8aabe12
dainbow <suslov9876@gmail.com>
2021-08-27 16:04:08 +0300

    Update and rename quadro.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <cassert>
+#include <stdarg.h>
+#include "globals.h"
+#include "funcs.cpp"
+
+
+int main() {
+#ifdef _USE_TESTS_
+    TestAll();
+#endif
+
+    printf("Solve quadratic equation\n");
+    printf("Please enter coefficients of the equation in format \"a b c\" (3 numbers)\n");
+
+    double a = NAN, b = NAN, c = NAN;
+    ReadCoeffs(&a, &b, &c);
+
+    double x1 = NAN, x2 = NAN;
+    int rootsCount = SolveQuad(a, b, c, &x1, &x2);
+
+    PrintAnswer(rootsCount, &x1, &x2);
+
+    return 0;
+}
+
